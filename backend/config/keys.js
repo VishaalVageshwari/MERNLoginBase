3a0dd585015b7dfb3cc0dfcc4152d78141757db5
VishaalVageshwari <37010747+VishaalVageshwari@users.noreply.github.com>
2019-12-17 18:36:18 +1100

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://Vishaal:'+ encodeURIComponent('vishaal12') + '@cluster11-du6mi.mongodb.net/test?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://<user>:'+ encodeURIComponent('<password>') + '@cluster11-du6mi.mongodb.net/test?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword,
